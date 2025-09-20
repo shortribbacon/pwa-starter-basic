f707d33dcb7c90252e06d7d5a1e9ca48dd3b4f33
shortribbacon <MyEternalLifeLegacy@proton.me>
2025-09-20 12:30:28 -0400

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+// Basic service worker to cache files for offline use
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('myapp-cache').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
